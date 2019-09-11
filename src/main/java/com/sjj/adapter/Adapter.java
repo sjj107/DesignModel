b8005166a9f95908bd69c0b4ea41726662b18771
sjj107 <sjj107@users.noreply.github.com>
2019-09-11 20:54:36 +0800

    更新

---
+++ src/main/java/com/sjj/adapter/Adapter.java
@@ -0,0 +1,4 @@
+package com.sjj.adapter;
+
+public class Adapter {
+}
--- src/main/java/com/sjj/adapter/Demo.java
+++
@@ -1,7 +0,0 @@
-package com.sjj.adapter;
-
-public class Demo {
-    public static void main(String[] args) {
-        System.out.println("test");
-    }
-}
